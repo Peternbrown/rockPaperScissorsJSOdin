0e205cb9c4ef325eb994410e6adc2ada77532aab
Peter <peternbrown100@gmail.com>
2024-01-28 13:50:37 -0500

    Cleaned up code, swapped the item labeling from getComputerChoice function to the playRound function so it fell in scope. App fully functional.

--- rps.js
+++ rps.js
@@ -10,39 +10,46 @@ function getComputerChoice() {
     switch (getRandomInt(3)) {
         case 0:
             return 0;
-            computerChoiceWord = "rock";
-            break;
         case 1:
             return 1;
-            computerChoiceWord = "paper";
-            break;
         case 2:
             return 2;
-            computerChoiceWord = "scissors";
-            break;
     }
 };
 
 function playRound(computerSelection) {
     let playerSelection = prompt("Let's play Rock, Paper, Scissors!\n\nEnter 0 for rock\nEnter 1 for paper\nEnter 2 for scissors");
 
-    if (playerSelection === computerSelection) {
-        return concat("Its a Tie! You both chose ", computerChoiceWord, ". Play again!");
+    let item;
+    switch (true) {
+        case playerSelection == 0:
+            item = "rock";
+            break;
+        case playerSelection == 1:
+            item = "paper";
+            break;
+        case playerSelection == 2:
+            item = "scisors";
+            break;
+    }
+
+    if (playerSelection == computerSelection) {
+        return "Its a Tie! You both chose " + item + ". Play again!";
     } else if (playerSelection == 0 && computerSelection == 1) {
-        return "The computer chose paper. You lose!";
+        return "You chose " + item + ".The computer chose paper. You lose!";
     } else if (playerSelection == 0 && computerSelection == 2) {
-        return "The computer chose scissors. You win!";
+        return "You chose " + item + ".The computer chose scissors. You win!";
     } else if (playerSelection == 1 && computerSelection == 2) {
-        return "The computer chose scissors. You lose!";
+        return "You chose " + item + ".The computer chose scissors. You lose!";
     } else if (playerSelection == 1 && computerSelection == 0) {
-        return "The computer chose rock. You win!";
+        return "You chose " + item + ".The computer chose rock. You win!";
     } else if (playerSelection == 2 && computerSelection == 1) {
-        return "The computer chose paper. You win!";
+        return "You chose " + item + ".The computer chose paper. You win!";
     } else if (playerSelection == 2 && computerSelection == 0) {
-        return "The computer chose rock. You lose!";
+        return "You chose " + item + ". The computer chose rock. You lose!";
     };
 };
 
 let computerChoiceWord;
-const computerSelection = 0;
+const computerSelection = getComputerChoice();
 console.log(playRound(computerSelection));
